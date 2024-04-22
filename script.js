761ddd14f54f17df098c0deca9c42ddeb5df3577
contato.gustavoqlima <contato.gustavoqlima@gmail.com>
2024-04-21 22:37:49 -0300

    task add

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-function createTask() {}
-function saveTask() {}
+const localStorageName = "task-list";
+
+function createTask() {
+  let input = document.getElementById("input-add-task");
+
+  if (!input.value) {
+    alert("Por favor digite algo");
+  } else {
+    let values = JSON.parse(localStorage.getItem(localStorageName) || "[]");
+    values.push({
+      task: input.value,
+    });
+    localStorage.setItem(localStorageName, JSON.stringify(values));
+    showTasks();
+  }
+}
+
+function showTasks() {
+  let values = JSON.parse(localStorage.getItem(localStorageName) || "[]");
+  let list = document.getElementById("tasks-list");
+  list.innerHTML = "";
+  for (let i = 0; i < values.length; i++) {
+    list.innerHTML = `<li>${values[i]["task"]}</li>`;
+  }
+}
